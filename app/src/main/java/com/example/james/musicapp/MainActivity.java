8d424e6fc451988ccb90bff34ad0ad79bb3c0301
James <james82772203@gmail.com>
2018-04-05 01:03:34 -0400

    soundpool_test

--- app/src/main/java/com/example/james/musicapp/MainActivity.java
+++ app/src/main/java/com/example/james/musicapp/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.james.musicapp;
 
+import android.media.AudioAttributes;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.media.SoundPool;
 import android.nfc.Tag;
 import android.os.CountDownTimer;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -21,6 +24,14 @@ public class MainActivity extends AppCompatActivity {
     private Button button_4;
     private Button Button_metronome;
 
+
+    int streamMetronome;
+
+
+
+
+    private boolean isMetronome = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,38 +42,62 @@ public class MainActivity extends AppCompatActivity {
         button_3 = (Button)findViewById(R.id.Button_3);
         button_4 = (Button)findViewById(R.id.Button_4);
 
+//        AudioAttributes test =
+//                new AudioAttributes.Builder().setContentType(AudioAttributes.CONTENT_TYPE_MUSIC).setUsage(AudioAttributes.USAGE_GAME).build();
 
-        final MediaPlayer metronome = MediaPlayer.create(MainActivity.this,R.raw.hihat_1);
+        final SoundPool soundPool_test = new SoundPool.Builder().setMaxStreams(10).build();
+
+        final int soundID_1 = soundPool_test.load(MainActivity.this,R.raw.hihat_1,1);
+        final int soundID_2 = soundPool_test.load(MainActivity.this,R.raw.drum_1,1);
+        final int soundID_3 = soundPool_test.load(MainActivity.this,R.raw.drum_2,1);
+        final int soundID_4 = soundPool_test.load(MainActivity.this,R.raw.bassdrum_1,1);
+        final int soundID_5 = soundPool_test.load(MainActivity.this,R.raw.hihat_2,1);
+
+
+
         Button_metronome = (Button)findViewById(R.id.Button_metronome);
         Button_metronome.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Log.d(TAG, "onClick: metronome");
-
-                if (metronome.isPlaying())
+                if(!isMetronome)
                 {
-                    metronome.setLooping(false);
+                    float soundRate = (float)1.0;
+                    Log.d(TAG, "onClick: start");
+                    isMetronome = true;
+                    streamMetronome = soundPool_test.play(soundID_1,1,1,1,-1,soundRate);
+//                    thread_metronome.start();
+//                    metronome.start();
+//                    metronome.setLooping(true);
+
                 }
                 else
                 {
-                    metronome.start();
-                    metronome.setLooping(true);
+                    Log.d(TAG, "onClick: stop");
+                    isMetronome = false;
+                    soundPool_test.stop(streamMetronome);
+//                    metronome.stop();
+//                    metronome.setLooping(false);
+//                    metronome.prepareAsync();
                 }
+
             }
         });
+
 
 
         button_1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Log.d(TAG, "onClick: Button1");
-                MediaPlayer drum_1 = MediaPlayer.create(MainActivity.this,R.raw.hihat_3);
-                drum_1.start();
-                drum_1.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-                    public void onCompletion(MediaPlayer mp) {
-                        mp.release();
-                    };
-                });
+                soundPool_test.play(soundID_1,1,1,1,0,1);
+//                MediaPlayer drum_1 = MediaPlayer.create(MainActivity.this,R.raw.hihat_2);
+//                drum_1.start();
+//                drum_1.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+//                    public void onCompletion(MediaPlayer mp) {
+//                        mp.release();
+//                    };
+//                });
             }
         });
 
@@ -70,13 +105,15 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Log.d(TAG, "onClick: Button2");
-                MediaPlayer drum_2 = MediaPlayer.create(MainActivity.this,R.raw.drum_2);
-                drum_2.start();
-                drum_2.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-                    public void onCompletion(MediaPlayer mp) {
-                        mp.release();
-                    };
-                });
+                soundPool_test.play(soundID_2,1,1,1,0,1);
+
+//                MediaPlayer drum_2 = MediaPlayer.create(MainActivity.this,R.raw.drum_1);
+//                drum_2.start();
+//                drum_2.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+//                    public void onCompletion(MediaPlayer mp) {
+//                        mp.release();
+//                    };
+//                });
             }
         });
 
@@ -84,13 +121,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Log.d(TAG, "onClick: Button3");
-                MediaPlayer drum_3 = MediaPlayer.create(MainActivity.this,R.raw.drum_2);
-                drum_3.start();
-                drum_3.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-                    public void onCompletion(MediaPlayer mp) {
-                        mp.release();
-                    };
-                });
+                soundPool_test.play(soundID_3,1,1,1,0,1);
+//                MediaPlayer drum_3 = MediaPlayer.create(MainActivity.this,R.raw.drum_2);
+//                drum_3.start();
+//                drum_3.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+//                    public void onCompletion(MediaPlayer mp) {
+//                        mp.release();
+//                    };
+//                });
             }
         });
 
@@ -98,13 +136,15 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Log.d(TAG, "onClick: Button4");
-                MediaPlayer drum_4 = MediaPlayer.create(MainActivity.this,R.raw.bassdrum_2);
-                drum_4.start();
-                drum_4.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-                    public void onCompletion(MediaPlayer mp) {
-                        mp.release();
-                    };
-                });
+                soundPool_test.play(soundID_4,1,1,1,0,1);
+
+//                MediaPlayer drum_4 = MediaPlayer.create(MainActivity.this,R.raw.bassdrum_2);
+//                drum_4.start();
+//                drum_4.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+//                    public void onCompletion(MediaPlayer mp) {
+//                        mp.release();
+//                    };
+//                });
             }
         });
     }
